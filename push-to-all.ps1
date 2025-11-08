3619a1ca4456f27d4beff515c8ad546ade5ac0d2
lbk168 <103929769@qq.com>
2025-11-08 21:07:52 +0800

    scripts: Update automation scripts - 2025-11-08 21:07:49

--- push-to-all.ps1
+++ push-to-all.ps1
@@ -135,7 +135,7 @@ function Push-ToRemotes {
     Write-ColorOutput "`nStarting push to all remote repositories..." "Yellow"
     
     # Define remote repositories explicitly
-    $remoteNames = @("gitee", "github")
+    $remoteNames = @("origin", "github")
     foreach ($remote in $remoteNames) {
         Write-ColorOutput "Pushing to $remote..." "Yellow"
         git push $remote master
